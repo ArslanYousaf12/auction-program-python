944cd8f9d4ba983511cb7a5f2f2b4ffd704848f6
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-21 12:44:56 +0500

    dictionary challenge 2 start

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
-# 🚨 Don't change the code above 👆
-# Scores 91 - 100: Grade = "Outstanding"
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
 
-# Scores 81 - 90: Grade = "Exceeds Expectations"
-
-# Scores 71 - 80: Grade = "Acceptable"
-
-# Scores 70 or lower: Grade = "Fail"
-#TODO-1: Create an empty dictionary called student_grades.
-student_grades = {}
-
-#TODO-2: Write your code below to add the grades to student_grades.👇
-for name in student_scores:
-    score = student_scores[name]
-    print(name)
-    print(score)
-    if score >= 90 and score < 100:
-        student_grades[name] = "Outstanding"
-    elif score >= 81 and score <= 90:
-        student_grades[name] = "Exceeds Expectations"
-    elif score >= 71 and score <= 80:
-        student_grades[name] = "Acceptable"
-    elif score <= 70:
-        student_grades[name] = "Fail"
-    
-    
-    
-
-# 🚨 Don't change the code below 👇
-print(student_grades)
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
 
 
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
 
 
 
