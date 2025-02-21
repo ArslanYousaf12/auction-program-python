3e5de6e404cef5d8997a000953838a9ec5124e13
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-21 13:25:22 +0500

    auction program complete

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-travel_log = [
-{
-  "country": "France",
-  "visits": 12,
-  "cities": ["Paris", "Lille", "Dijon"]
-},
-{
-  "country": "Germany",
-  "visits": 5,
-  "cities": ["Berlin", "Hamburg", "Stuttgart"]
-},
-]
-#🚨 Do NOT change the code above
-
-#TODO: Write the function that will allow new countries
-#to be added to the travel_log. 👇
-def add_new_country(country_visited, total_visit, cities):
-    new_entry = {
-        "country": country_visited,
-        "visit": total_visit,
-        "cities": cities
-        }
-    travel_log.append(new_entry)
+import os
+from art import logo
+bider_info = {}
+biders = []
+print(logo)
+print("Welcome to the Secret Auction Program")
+end_of_auction = True
+highest_bider_name = ""
+highest_bid_amount = 0
+while end_of_auction:
+    bidder_name = input("what is your name ")
+    bid_amount = int(input("what is your bid "))
 
 
+    bider_info = {"name": bidder_name, "amount": bid_amount}
 
+    biders.append(bider_info)
 
+    result = input("Is there any bidder 'yes' or 'no' ")
+    if result == "yes":
+        os.system('clear')
+    else:
+        end_of_auction = False
 
-#🚨 Do not change the code below
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log)
+print(biders)
+
+highest_bider_name = biders[0]["name"]
+highest_bid_amount = biders[0]["amount"]
+for bidder in biders:
+    amount = bidder["amount"]
+    name = bidder["name"]
+    if amount > highest_bid_amount:
+        highest_bid_amount = amount
+        highest_bider_name = name
+
+print(f"Highest bidder is {highest_bider_name} and bid is {highest_bid_amount}")
 
 
 
