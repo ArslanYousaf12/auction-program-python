93ea5d3c10320d6ed702bea523827a1da8b715c7
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-21 11:57:38 +0500

    just learn the dictionary

---
+++ main.py
@@ -0,0 +1,17 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+    }
+programming_dictionary["loop"] = "iterative structure that repeat statement or set of statement"
+print(programming_dictionary)
+# empty dictionary
+empty_dictionary = {}
+
+# wipe the Dictionary
+
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
