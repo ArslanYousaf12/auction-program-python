3b8d720eb4a32221c5f1c095d142eed8262b61f2
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-21 13:42:16 +0500

    refactor the code

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 import os
 from art import logo
-bider_info = {}
-biders = []
+
 print(logo)
 print("Welcome to the Secret Auction Program")
 end_of_auction = True
-highest_bider_name = ""
-highest_bid_amount = 0
+
+bid = {}
+
+def find_highest_bidder(bid):
+    highest_bider_amount = 0
+    winner = ""
+    for bidder in bid:
+        amount = bid[bidder]
+        if amount > highest_bider_amount:
+            highest_bider_amount = amount
+            winner = bidder
+    print(f"higest bider is {winner} and highest bid is {highest_bider_amount}")
 while end_of_auction:
     bidder_name = input("what is your name ")
     bid_amount = int(input("what is your bid "))
-
-
-    bider_info = {"name": bidder_name, "amount": bid_amount}
-
-    biders.append(bider_info)
-
+    bid[bidder_name] = bid_amount
     result = input("Is there any bidder 'yes' or 'no' ")
     if result == "yes":
         os.system('clear')
     else:
         end_of_auction = False
+        find_highest_bidder(bid=bid)
 
-print(biders)
+print(bid)
 
-highest_bider_name = biders[0]["name"]
-highest_bid_amount = biders[0]["amount"]
-for bidder in biders:
-    amount = bidder["amount"]
-    name = bidder["name"]
-    if amount > highest_bid_amount:
-        highest_bid_amount = amount
-        highest_bider_name = name
 
-print(f"Highest bidder is {highest_bider_name} and bid is {highest_bid_amount}")
+# print(f"Highest bidder is {highest_bider_name} and bid is {highest_bid_amount}")
 
 
 
